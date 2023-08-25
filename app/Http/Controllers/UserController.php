7b9b63a92811ea1489e0b4ad8ad7550d0b33f5d1
Segun <miksegun1996@gmail.com>
2023-08-25 02:56:09 +0100

    updated the login method to phone and password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,12 +26,12 @@ class UserController extends Controller
     public function login(Request $request)
     {
         
-        $input = $request->only('email', 'password');
+        $input = $request->only('phone', 'password');
         $jwt_token = null;
         if (!$jwt_token = JWTAuth::attempt($input)) {
             return response()->json([
                 'success' => false,
-                'message' => 'Invalid Email or Password',
+                'message' => 'Invalid Phone or Password',
             ], 401);
         }
         // get the user 
